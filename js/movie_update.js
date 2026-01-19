f2abb55afbcd18802044dfbb09cb5d99af77000c
Liam <cxl824483863@gmail.com>
2026-01-19 10:28:25 +0800

    Update movie_update.js

--- js/movie_update.js
+++ js/movie_update.js
@@ -5,15 +5,16 @@
 const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MmUwNTI4NWM5YjYxMWI3MjhlOTYzZmM3ZjNiYjk2YiIsIm5iZiI6MTc2ODQwMDcyMi42MTc5OTk4LCJzdWIiOiI2OTY3YTc1MmVhZjg5YzIwMmE4NjY1NDMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.crwmHTGpE_x5azP_O2lx6BaJt74Gk900XcR2A9Fvml4";
 
 const SHOWS = [
+  { id: 30984,  name: "死神：千年血战", category: "日漫" },
+  { id: 222766, name: "豺狼的日子", category: "美剧"},
+  { id: 224372, name: "七王国的骑士", category: "美剧" },
   { id: 106379, name: "辐射", category: "美剧" },
   { id: 101172, name: "吞噬星空", category: "国漫" },
   { id: 67063,  name: "一人之下", category: "国漫" },
-  { id: 30984,  name: "死神：千年血战", category: "日漫" },
   { id: 209867, name: "葬送的芙莉莲", category: "日漫"},
   { id: 117465, name: "地狱乐", categorg: "日漫"},
   { id: 249907, name: "判处勇者刑 刑罚勇者9004队服刑记录", category: "日漫" },
-  { id: 139060, name: "魔都精兵的奴隶", category: "日漫" },
-  { id: 222766, name: "豺狼的日子", category: "美剧"} 
+  { id: 139060, name: "魔都精兵的奴隶", category: "日漫" }
 ];
 
 const UPCOMING_DAYS = 7;
